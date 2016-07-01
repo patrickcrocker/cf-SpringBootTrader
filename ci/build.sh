9fb4ae58ae8003ab08f758e6bd15b8eaf2448fe9
patrickcrocker <patrick.crocker@gmail.com>
2016-06-30 19:58:26 -0500

    added ci folder

---
+++ ci/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+VERSION=`cat version/number`
+
+pushd project
+  ./gradlew -PversionNumber=$VERSION clean assemble
+popd
+
+cp project/build/libs/$ARTIFACT_ID-$VERSION.jar build/.

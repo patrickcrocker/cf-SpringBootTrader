eb184380ab010b3b779c24fadbe4296208674840
patrickcrocker <patrick.crocker@gmail.com>
2016-11-03 17:11:03 -0500

    check for service availability

--- ci/prepare-cf.sh
+++ ci/prepare-cf.sh
@@ -4,6 +4,38 @@
 
 set -e -x
 
+SERVICE_PROVISION_TIMEOUT=${SERVICE_PROVISION_TIMEOUT:-500}
+
+function wait_for_service_instance() {
+  service_instance=$1
+
+  guid=`cf service ${service_instance} --guid`
+
+  start=`date +%s`
+  while true; do
+    # Get the service instance info in JSON from CC and parse out the async 'state'
+    state=`cf curl /v2/service_instances/${guid} | jq -r .entity.last_operation.state`
+
+    if [ "$state" == "succeeded" ]; then
+      echo "Service ${service_instance} is ready"
+      return
+    elif [ "$state" == "failed" ]; then
+      echo "Service ${service_instance} failed to provision"
+      cf delete-service -f $service_instance
+      exit 1
+    fi
+
+    now=`date +%s`
+    time=$(($now - $start))
+    if [[ "$time" -ge "$SERVICE_PROVISION_TIMEOUT" ]]; then
+      echo "Timed out waiting for service instance to provision: ${service_instance}"
+      cf delete-service -f $service_instance
+      exit 1
+    fi
+    sleep 5
+  done
+}
+
 if [ "true" = "$CF_SKIP_SSL" ]; then
   cf api $CF_API_URL --skip-ssl-validation
 else
@@ -50,19 +82,19 @@ fi
 HAS_SERVICE=$(cf services | grep config-server || true)
 if [ -z "$HAS_SERVICE" ]; then
   cf create-service p-config-server standard config-server -c "{ \"git\": { \"uri\": \"$CF_CONFIG_SERVER_URI\", \"label\": \"$CF_CONFIG_SERVER_LABEL\" } }"
-  SLEEP=60
+  wait_for_service_instance config-server
 fi
 
 HAS_SERVICE=$(cf services | grep discovery-service || true)
 if [ -z "$HAS_SERVICE" ]; then
   cf create-service p-service-registry standard discovery-service
-  SLEEP=70
+  wait_for_service_instance discovery-service
 fi
 
 HAS_SERVICE=$(cf services | grep circuit-breaker-dashboard || true)
 if [ -z "$HAS_SERVICE" ]; then
   cf create-service p-circuit-breaker-dashboard standard circuit-breaker-dashboard
-  SLEEP=110
+  wait_for_service_instance circuit-breaker-dashboard
 fi
 
 # give services time to spin up
